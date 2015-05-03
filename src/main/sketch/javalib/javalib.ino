77fba19f3ee3200d62f43b50ae3bfe610b0900fe
skoky <skokys@gmail.com>
2015-05-03 11:43:21 +0200

    comms with openCM working!

--- src/main/sketch/javalib/javalib.ino
+++ src/main/sketch/javalib/javalib.ino
@@ -10,18 +10,20 @@ Dynamixel Dxl(DXL_BUS_SERIAL1);
 byte servo;
 byte address;
 byte b;
+byte command;
 int i;
 
 void setup() {
   // setup communication to servos, speed 1Mb
   Dxl.begin(3);
-  Dxl.jointMode(1);
-// Dxl.wheelMode(1); 
+//  Dxl.jointMode(1);
+ Dxl.wheelMode(1);
 
   SerialUSB.attachInterrupt(usbInterrupt);
   pinMode(BOARD_LED_PIN, OUTPUT);  //toggleLED_Pin_Out
 
-  Dxl.maxTorque(J_ID,1); // it has maxtorque for weak movement  
+  Dxl.maxTorque(J_ID,1); // it has maxtorque for weak movement
+  command=0;
 }
 
 //USB max packet data is maximum 64byte, so nCount can not exceeds 64 bytes
@@ -33,50 +35,61 @@ void setup() {
 void usbInterrupt(byte* buffer, byte nCount) {
 
   if (nCount!=5) {
-    SerialUSB.println(-2);
+    SerialUSB.println(100);
     return;
   }
 
   servo=buffer[1];
   address=buffer[2];
-  switch (buffer[0]) {
+  command=buffer[0];
+  switch (command) {
   case 1:  // write byte
     Dxl.writeByte(servo, address, buffer[3]);
     SerialUSB.println(buffer[3]);
     break;
   case 2:  // read byte
-    b = Dxl.readByte(servo,address);
-    SerialUSB.println(b);
+//    b = Dxl.readByte(servo,address);
+//    SerialUSB.println(b);
     break;
   case 3:  // write word
-    i = buffer[3];  // FIXME
+    i = buffer[3]*255;  
+    i = i + buffer[4];
     Dxl.writeWord(servo, address, i);      
     SerialUSB.println(i);
     break;
   case 4:  // read word
-    i = Dxl.readWord(servo,address);
-    int pos;
-    pos = Dxl.readWord(J_ID, PRESENT_POS);
-    SerialUSB.println(pos);
+    // result will be picked by loop
+    // i = Dxl.readWord(servo,address);
+    // i = Dxl.readWord(J_ID, PRESENT_POS);
+    // SerialUSB.println(i);
     break;
   default: 
-    buffer[0]=11;
-    SerialUSB.println(-1);
+    SerialUSB.println(101);
   } 
 }
 
 void loop(){
-  toggleLED();
+//  toggleLED();
 //  int pos;
 //  pos = Dxl.readWord(J_ID, PRESENT_POS); // Read present position
 //  SerialUSB.print("Present Position: ");
 //  SerialUSB.println(pos);
 
-  delay(1000);
+//  delay(1000);
 //  Dxl.writeWord(1, 30, 0);
 //  delay(1000);
 //  Dxl.writeWord(1, 30, 99);
-  
+
+  if (command==4) {
+    i = Dxl.readWord(servo,address);
+    SerialUSB.println(i);
+    command=0;      
+  } else if (command==2) {
+    b = Dxl.readByte(servo,address);
+    SerialUSB.println(b);
+    command=0;    
+  }
+
 }
 
 
