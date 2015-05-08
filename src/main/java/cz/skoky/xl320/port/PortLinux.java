bf5efd9d503240ea72b0d7e15d86af7362e1ccf7
Laco Skokan <ladislav.skokan@wincor-nixdorf.cz>
2015-05-08 17:08:22 +0200

    crc calculation

---
+++ src/main/java/cz/skoky/xl320/port/PortLinux.java
@@ -0,0 +1,51 @@
+package cz.skoky.xl320.port;
+
+import jtermios.*;
+
+import java.util.List;
+
+import static jtermios.JTermios.*;
+/**
+ * Created by skokan on 7.5.15.
+ */
+public class PortLinux implements SerialPort {
+
+    @Override
+    public void open(String portName) {
+        int fd = JTermios.open(portName, O_RDWR | O_NOCTTY | O_NONBLOCK);
+        if (fd == -1)
+            throw new RuntimeException("Unale to open port "+portName);
+        fcntl(fd, F_SETFL, 0);
+
+        Termios opts = new Termios();
+        tcgetattr(fd, opts);
+        opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+        opts.c_cflag |= (CLOCAL | CREAD);
+        opts.c_cflag &= ~PARENB;
+        opts.c_cflag |= CSTOPB;
+        opts.c_cflag &= ~CSIZE;
+        opts.c_cflag |= CS8;
+        opts.c_oflag &= ~OPOST;
+        opts.c_iflag &= ~INPCK;
+        opts.c_iflag &= ~(IXON | IXOFF | IXANY);
+        opts.c_cc[VMIN] = 0;
+        opts.c_cc[VTIME] = 10;
+
+        cfsetispeed(opts, B9600);
+        cfsetospeed(opts, B9600);
+
+        tcsetattr(fd, TCSANOW, opts);
+
+        tcflush(fd, TCIOFLUSH);
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public List<String> getPortsList() {
+        return JTermios.getPortList();
+    }
+}
