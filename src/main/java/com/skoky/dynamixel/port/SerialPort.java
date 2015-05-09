a69e38500dd0d818465146733ae031ea355985b3
skoky <skokys@gmail.com>
2015-05-09 11:20:52 +0200

    header fix and factory

--- src/main/java/com/skoky/dynamixel/port/SerialPort.java
+++ src/main/java/com/skoky/dynamixel/port/SerialPort.java
@@ -1,13 +1,19 @@
 package com.skoky.dynamixel.port;
 
+import com.skoky.dynamixel.raw.Packet;
+import jtermios.JTermios;
+
 import java.util.List;
 
 /**
  * Created by skokan on 7.5.15.
  */
 public interface SerialPort {
-    List<String> getPortsList();
-    void open(String portName);
+    static List<String> getPortsList() {
+        return JTermios.getPortList();
+    }
     void close();
 
+
+    byte[] sendAndReceive(Packet p);
 }
--- src/main/java/com/skoky/dynamixel/raw/PacketV1.java
+++ src/main/java/com/skoky/dynamixel/raw/PacketV1.java
@@ -6,12 +6,13 @@ package com.skoky.dynamixel.raw;
 public class PacketV1 implements Packet {
     @Override
     public byte[] buildPing() {
-        int[] buffer = new int[5];
+        int[] buffer = new int[6];
         buffer[0]= 0xFF; //header
-        buffer[1]= 0xFE; //servo ID
-        buffer[2]=2;            // length
-        buffer[3]=0x1;          // PING
-        buffer[4] = crc(buffer);    // CRC
+        buffer[1]= 0xFF; //header
+        buffer[2]= 0xFE; //servo ID
+        buffer[3]=2;            // length
+        buffer[4]=0x1;          // PING
+        buffer[5] = crc(buffer);    // CRC
         return toByteArray(buffer);
     }
 
@@ -25,23 +26,31 @@ public class PacketV1 implements Packet {
 
     @Override
     public byte[] buildWriteDate(int servoId, int... params) {
-        int[] buffer = new int[8];
+        int[] buffer = new int[6+params.length];
         buffer[0]= 0xFF; //header
-        buffer[1]= servoId; //servo ID
-        buffer[2]= 0x05;            // length
-        buffer[3]= 0x3;          // WRITE_DATA
+        buffer[1]= 0xFF; //header
+        buffer[2]= servoId; //servo ID
+        buffer[3]= 0x05;            // length
+        buffer[4]= 0x3;          // WRITE_DATA
         for(int i=0;i<params.length;i++) {
-            buffer[4+i] = params[i];
+            buffer[5+i] = params[i];
         }
-        buffer[4+params.length] = crc(buffer,params);    // CRC
+        buffer[5+params.length] = crc(buffer,params);    // CRC
         return toByteArray(buffer);
+    }
+
+    @Override
+    public void parse(byte[] p) {
+        if (p[0]!=0xFF) throw new IllegalArgumentException("Not starting with 0xFF");
+
+
     }
 
 
     private int crc(int[] buffer, int... params) {
-        int sum = buffer[1];
-        sum +=buffer[2];
+        int sum = buffer[2];
         sum +=buffer[3];
+        sum +=buffer[4];
         for(int i=0;i<params.length;i++) {
             sum += params[i];
         }
