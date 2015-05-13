50a2e474b486ce694b7ffa9bc3cc0bd8d0331c92
skoky <skokys@gmail.com>
2015-05-13 20:19:55 +0200

    ping with AX12A

--- src/main/java/com/skoky/dynamixel/servo/ServoAX12A.java
+++ src/main/java/com/skoky/dynamixel/servo/ServoAX12A.java
@@ -1,11 +1,21 @@
 package com.skoky.dynamixel.servo;
 
+import com.skoky.dynamixel.Controller;
 import com.skoky.dynamixel.Servo;
+import com.skoky.dynamixel.controller.OpenCM;
 
 /**
  * Created by skoky on 5.5.15.
  */
 public class ServoAX12A implements Servo {
+    private final int servoId;
+    private final Controller controller;
+
+    public ServoAX12A(int servoId, Controller controller) {
+        this.servoId=servoId;
+        this.controller=controller;
+    }
+
     @Override
     public int getPresentPosition() {
         return 0;
@@ -29,5 +39,13 @@ public class ServoAX12A implements Servo {
     @Override
     public void setCWLimit(int i) {
 
+    }
+
+    @Override
+    public String toString() {
+        return "ServoAX12A{" +
+                "servoId=" + servoId +
+                ", controller=" + controller +
+                '}';
     }
 }
