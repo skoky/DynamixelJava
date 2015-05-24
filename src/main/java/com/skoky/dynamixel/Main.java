8cc339eb38edb17389b0eb380544ba859f686cc4
skoky <skokys@gmail.com>
2015-05-24 13:08:26 +0200

    scripts update

---
+++ src/main/java/com/skoky/dynamixel/Main.java
@@ -0,0 +1,11 @@
+package com.skoky.dynamixel;
+
+/**
+ * Created by skoky on 24.5.15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.err.println("Not implemented yet");
+    }
+}
